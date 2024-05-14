a7f73098b4855062cced50feb0c8d30351ad9335
Lukas <kosha.industry@gmail.com>
2024-05-14 10:01:42 +0500

    updates

--- lib/example/lib/bloc/flex_settings_cubit.dart
+++ lib/example/lib/bloc/flex_settings_cubit.dart
@@ -5,4 +5,20 @@ part 'flex_settings_state.dart';
 
 class FlexSettingsCubit extends Cubit<FlexSettingsState> {
   FlexSettingsCubit() : super(const FlexSettingsState());
+
+  set mainAxisAlignment(MainAxisAlignment value) {
+    emit(state.copyWith(mainAxisAlignment: value));
+  }
+
+  set mainAxisSize(MainAxisSize value) {
+    emit(state.copyWith(mainAxisSize: value));
+  }
+
+  set crossAxisAlignment(CrossAxisAlignment value) {
+    emit(state.copyWith(crossAxisAlignment: value));
+  }
+
+  set verticalDirection(VerticalDirection value) {
+    emit(state.copyWith(verticalDirection: value));
+  }
 }
