1d173fc1bd49c827c5421db90c337d8189d5ddea
Lukas <kosha.industry@gmail.com>
2024-05-14 08:32:26 +0500

    updates

---
+++ lib/elevated_flex_delegate.dart
@@ -0,0 +1,9 @@
+part of 'elevated_flex.dart';
+
+abstract class _ElevatedFlexDelegate extends BoxyDelegate {
+
+  double get sumChildrenHeight {
+    return children.fold(0.0, (sum, child) => sum + child.size.height);
+  }
+
+}
