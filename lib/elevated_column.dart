f051d8a6c2ab8a05b9c4de31ceda2faf3f8736a9
Lukas <kosha.industry@gmail.com>
2024-05-14 07:25:17 +0500

    add elevated_column.dart

---
+++ lib/elevated_column.dart
@@ -0,0 +1,70 @@
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:boxy/boxy.dart';
+
+part 'elevated.dart';
+part 'elevated_column_delegate.dart';
+
+/// Вижет который строит колонку
+/// при этом какие-то дети могут быть выше остальных с позиции z-index
+/// для этого нужно этих детей обернуть в [Elevated]
+class ElevatedColumn extends StatelessWidget {
+  const ElevatedColumn({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.verticalDirection = VerticalDirection.down,
+    this.children = const <Widget>[],
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final VerticalDirection verticalDirection;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children;
+    if (verticalDirection == VerticalDirection.down) {
+      children = this.children;
+    } else {
+      children = this.children.reversed.toList();
+    }
+
+    final Map<int, Widget> indexedChildren = children.asMap();
+
+    // sort children, move elevated widgets down
+    List<MapEntry<int, Widget>> sortedIndexedChildren = indexedChildren.entries.sorted((a, b) {
+      final aWidget = a.value;
+      final bWidget = b.value;
+
+      final aScore = aWidget is Elevated ? 1 : 0;
+      final bScore = bWidget is Elevated ? 1 : 0;
+
+      return aScore - bScore;
+    });
+
+    // convert children to BoxyId, but id saves their original order
+    final boxyChildren = sortedIndexedChildren.map((MapEntry<int, Widget> entry) {
+      final index = entry.key;
+      final child = entry.value;
+
+      return BoxyId(
+        id: index,
+        child: child,
+      );
+    }).toList();
+
+    return CustomBoxy(
+      delegate: _ElevatedColumnDelegate(
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        verticalDirection: verticalDirection,
+      ),
+      children: boxyChildren,
+    );
+  }
+}
