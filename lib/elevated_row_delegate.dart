c52c7820d8304250310cfb4f3ee27e58d9917f02
Lukas <kosha.industry@gmail.com>
2024-05-14 11:23:02 +0500

    updates

--- lib/elevated_row_delegate.dart
+++ lib/elevated_row_delegate.dart
@@ -10,11 +10,6 @@ class _ElevatedRowDelegate extends _ElevatedFlexDelegate {
           direction: Axis.horizontal,
         );
 
-  // inner calculation
-  late final int childrenCount;
-  late final double width;
-  late final double height;
-
   @override
   Size layout() {
     // remember children count
@@ -39,51 +34,6 @@ class _ElevatedRowDelegate extends _ElevatedFlexDelegate {
     positionChildren();
 
     return Size(width, height);
-  }
-
-  MainAxisAlignment get effectiveMainAxisAlignment {
-    // reverse mainAxisAlignment for up vertical direction
-    if (direction == Axis.vertical && verticalDirection == VerticalDirection.up) {
-      if (mainAxisAlignment == MainAxisAlignment.start) return MainAxisAlignment.end;
-      if (mainAxisAlignment == MainAxisAlignment.end) return MainAxisAlignment.start;
-    }
-
-    return mainAxisAlignment;
-  }
-
-  ({double startX, double stepShift}) calcStartXAndShift() {
-    switch (mainAxisSize) {
-      case MainAxisSize.min:
-        return (startX: 0, stepShift: 0);
-
-      case MainAxisSize.max:
-        double freeSpaceHeight = constraints.maxWidth - sumChildrenWidth;
-
-        return switch (effectiveMainAxisAlignment) {
-          MainAxisAlignment.start => (startX: 0, stepShift: 0),
-
-          // readability
-          MainAxisAlignment.end => (startX: freeSpaceHeight, stepShift: 0),
-
-          // readability
-          MainAxisAlignment.center => (startX: freeSpaceHeight / 2, stepShift: 0),
-
-          // readability
-          MainAxisAlignment.spaceBetween => (startX: 0, stepShift: freeSpaceHeight / (childrenCount - 1)),
-
-          // readability
-          MainAxisAlignment.spaceAround => () {
-              final spaceForChild = freeSpaceHeight / (childrenCount * 2);
-              return (startX: spaceForChild, stepShift: spaceForChild * 2);
-            }(),
-
-          // readability
-          MainAxisAlignment.spaceEvenly => () {
-              final spaceForChild = freeSpaceHeight / (childrenCount + 1);
-              return (startX: spaceForChild, stepShift: spaceForChild);
-            }(),
-        };
-    }
   }
 
   void positionChildren() {
