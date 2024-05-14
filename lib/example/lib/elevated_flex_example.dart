7dbb816146131b626f8c53ea9e96780dbe8ef114
Lukas <kosha.industry@gmail.com>
2024-05-14 07:59:09 +0500

    example

--- lib/example/lib/elevated_flex_example.dart
+++ lib/example/lib/elevated_flex_example.dart
@@ -93,6 +93,12 @@ class _ExampleContainer extends StatelessWidget {
   }
 }
 
+const _shadow = BoxShadow(
+  blurRadius: 5,
+  spreadRadius: 5,
+  color: Colors.black87,
+);
+
 class _Item1 extends StatelessWidget {
   const _Item1();
 
@@ -105,27 +111,9 @@ class _Item1 extends StatelessWidget {
       alignment: Alignment.center,
       decoration: const BoxDecoration(
         color: Colors.cyan,
+        boxShadow: [_shadow],
       ),
       child: const Text('item1 (want top)', style: TextStyle(fontSize: 10), textAlign: TextAlign.center),
-    );
-  }
-}
-
-class _ItemCenter extends StatelessWidget {
-  const _ItemCenter();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      height: 100,
-      width: 100,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: Colors.pinkAccent.withOpacity(0.5),
-        border: Border.all(color: Colors.red, width: 10, strokeAlign: BorderSide.strokeAlignOutside),
-      ),
-      child: const Text('center'),
     );
   }
 }
@@ -142,8 +130,28 @@ class _Item2 extends StatelessWidget {
       alignment: Alignment.center,
       decoration: const BoxDecoration(
         color: Colors.lightGreen,
+        boxShadow: [_shadow],
       ),
       child: const Text('item2 (want top)', style: TextStyle(fontSize: 10), textAlign: TextAlign.center),
+    );
+  }
+}
+
+class _ItemCenter extends StatelessWidget {
+  const _ItemCenter();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 100,
+      width: 100,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        border: Border.all(color: Colors.red, width: 10, strokeAlign: BorderSide.strokeAlignOutside),
+      ),
+      child: const Text('center'),
     );
   }
 }
