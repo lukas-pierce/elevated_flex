4949f3535a7ed557b6a668b40f382788f1f4ff7a
Lukas <kosha.industry@gmail.com>
2024-05-14 07:23:36 +0500

    add example

--- lib/example/lib/elevated_flex_example.dart
+++ lib/example/lib/elevated_flex_example.dart
@@ -6,9 +6,85 @@ class ElevatedFlexExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const MainAxisAlignment mainAxisAlignment = MainAxisAlignment.spaceEvenly;
+    const MainAxisSize mainAxisSize = MainAxisSize.max;
+    const CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center;
+    const VerticalDirection verticalDirection = VerticalDirection.down;
+
     return const Scaffold(
-      body: Center(
-        child: Text('example'),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    _Example(
+                      child: Column(
+                        mainAxisAlignment: mainAxisAlignment,
+                        mainAxisSize: mainAxisSize,
+                        crossAxisAlignment: crossAxisAlignment,
+                        verticalDirection: verticalDirection,
+                        children: [
+                          Elevated(child: _Item1()),
+                          _ItemCenter(),
+                          Elevated(child: _Item2()),
+                        ],
+                      ),
+                    ),
+                    _Example(
+                      child: ElevatedColumn(
+                        mainAxisAlignment: mainAxisAlignment,
+                        mainAxisSize: mainAxisSize,
+                        crossAxisAlignment: crossAxisAlignment,
+                        verticalDirection: verticalDirection,
+                        children: [
+                          Elevated(child: _Item1()),
+                          _ItemCenter(),
+                          Elevated(child: _Item2()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(height: 0),
+              Expanded(
+                child: Row(
+                  children: [
+                    _Example(
+                      child: Column(
+                        mainAxisAlignment: mainAxisAlignment,
+                        mainAxisSize: mainAxisSize,
+                        crossAxisAlignment: crossAxisAlignment,
+                        verticalDirection: verticalDirection,
+                        children: [
+                          Elevated(child: _Item1()),
+                          _ItemCenter(),
+                          Elevated(child: _Item2()),
+                        ],
+                      ),
+                    ),
+                    _Example(
+                      child: ElevatedColumn(
+                        mainAxisAlignment: mainAxisAlignment,
+                        mainAxisSize: mainAxisSize,
+                        crossAxisAlignment: crossAxisAlignment,
+                        verticalDirection: verticalDirection,
+                        children: [
+                          Elevated(child: _Item1()),
+                          _ItemCenter(),
+                          Elevated(child: _Item2()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
