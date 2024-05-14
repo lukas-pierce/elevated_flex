2170b1b33e5255a86f5c954836f84aa39aee2bf5
Lukas <kosha.industry@gmail.com>
2024-05-14 11:47:12 +0500

    updates

--- lib/elevated_column_delegate.dart
+++ lib/elevated_column_delegate.dart
@@ -9,28 +9,4 @@ class _ElevatedColumnDelegate extends _ElevatedFlexDelegate {
   }) : super(
           direction: Axis.vertical,
         );
-
-  /// positioning children with order according elevation
-  @override
-  void positionChildren() {
-    final startAndShift = calcStartYAndShift();
-    double nextChildY = startAndShift.startY;
-
-    for (int i = 0; i < childrenCount; i++) {
-      final child = getChild(i);
-
-      // for column
-      final double y = nextChildY;
-      final double x = switch (crossAxisAlignment) {
-        CrossAxisAlignment.start || CrossAxisAlignment.stretch || CrossAxisAlignment.baseline => 0,
-        CrossAxisAlignment.end => width - child.size.width,
-        CrossAxisAlignment.center => (width - child.size.width) / 2,
-      };
-
-      child.position(Offset(x, y));
-
-      // calc y offset for next child
-      nextChildY += child.size.height + startAndShift.stepShift;
-    }
-  }
 }
--- lib/elevated_flex_delegate.dart
+++ lib/elevated_flex_delegate.dart
@@ -73,7 +73,53 @@ abstract class _ElevatedFlexDelegate extends BoxyDelegate {
     }
   }
 
-  void positionChildren();
+  /// positioning children with order according elevation
+  void positionChildren() {
+    if (direction == Axis.vertical) {
+
+      // for column
+      final startAndShift = calcStartYAndShift();
+      double nextChildY = startAndShift.startY;
+
+      for (int i = 0; i < childrenCount; i++) {
+        final child = getChild(i);
+
+        final double y = nextChildY;
+        final double x = switch (crossAxisAlignment) {
+          CrossAxisAlignment.start || CrossAxisAlignment.stretch || CrossAxisAlignment.baseline => 0,
+          CrossAxisAlignment.end => width - child.size.width,
+          CrossAxisAlignment.center => (width - child.size.width) / 2,
+        };
+
+        child.position(Offset(x, y));
+
+        // calc y offset for next child
+        nextChildY += child.size.height + startAndShift.stepShift;
+      }
+
+    } else {
+
+      // for row
+      final startAndShift = calcStartXAndShift();
+      double nextChildX = startAndShift.startX;
+
+      for (int i = 0; i < childrenCount; i++) {
+        final child = getChild(i);
+
+        final double x = nextChildX;
+        final double y = switch (crossAxisAlignment) {
+          CrossAxisAlignment.start || CrossAxisAlignment.stretch || CrossAxisAlignment.baseline => 0,
+          CrossAxisAlignment.end => height - child.size.height,
+          CrossAxisAlignment.center => (height - child.size.height) / 2,
+        };
+
+        child.position(Offset(x, y));
+
+        // calc y offset for next child
+        nextChildX += child.size.width + startAndShift.stepShift;
+      }
+    }
+  }
 
   MainAxisAlignment get effectiveMainAxisAlignment {
     // reverse mainAxisAlignment for up vertical direction
