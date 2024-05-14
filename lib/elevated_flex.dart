ada9057415b2f5e7b8922108b997cd0627e4b4a4
Lukas <kosha.industry@gmail.com>
2024-05-14 07:27:26 +0500

    add elevated.dart

--- lib/elevated_flex.dart
+++ lib/elevated_flex.dart
@@ -1,7 +1,5 @@
 library elevated_flex;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'package:flutter/material.dart';
+
+part 'elevated.dart';
