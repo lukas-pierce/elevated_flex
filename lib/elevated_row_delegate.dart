9f1922e7bb5083b95a8af1e91d14eb290a05d6fa
Lukas <kosha.industry@gmail.com>
2024-05-14 08:48:37 +0500

    updates

--- lib/elevated_row_delegate.dart
+++ lib/elevated_row_delegate.dart
@@ -30,7 +30,11 @@ class _ElevatedRowDelegate extends _ElevatedFlexDelegate {
       final child = getChild(i);
       BoxConstraints childConstraints = constraints;
       if (crossAxisAlignment == CrossAxisAlignment.stretch) {
-        childConstraints = constraints.copyWith(minWidth: constraints.maxWidth);
+        if (direction == Axis.vertical) {
+          childConstraints = constraints.copyWith(minWidth: constraints.maxWidth);
+        } else {
+          childConstraints = constraints.copyWith(minHeight: constraints.maxHeight);
+        }
       }
       child.layout(childConstraints);
     }
