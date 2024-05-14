184b10ceca026dc510dac25e1f2806853972f9b4
Lukas <kosha.industry@gmail.com>
2024-05-14 13:39:40 +0500

    updates

--- lib/example/lib/data/settings.dart
+++ lib/example/lib/data/settings.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/material.dart';
 
-import '../bloc/flex_settings_cubit.dart';
+@immutable
+final class FlexSettingsState {
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final VerticalDirection verticalDirection;
+
+  const FlexSettingsState({
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.min,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.verticalDirection = VerticalDirection.down,
+  });
+
+  FlexSettingsState copyWith({
+    MainAxisAlignment? mainAxisAlignment,
+    MainAxisSize? mainAxisSize,
+    CrossAxisAlignment? crossAxisAlignment,
+    VerticalDirection? verticalDirection,
+  }) {
+    return FlexSettingsState(
+      mainAxisAlignment: mainAxisAlignment ?? this.mainAxisAlignment,
+      mainAxisSize: mainAxisSize ?? this.mainAxisSize,
+      crossAxisAlignment: crossAxisAlignment ?? this.crossAxisAlignment,
+      verticalDirection: verticalDirection ?? this.verticalDirection,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is FlexSettingsState &&
+        other.mainAxisAlignment == mainAxisAlignment &&
+        other.mainAxisSize == mainAxisSize &&
+        other.crossAxisAlignment == crossAxisAlignment &&
+        other.verticalDirection == verticalDirection;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    mainAxisAlignment,
+    mainAxisSize,
+    crossAxisAlignment,
+    verticalDirection,
+  );
+}
 
 class FlexSettings extends InheritedWidget {
   const FlexSettings({
