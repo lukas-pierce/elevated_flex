a334dfeda70468851cd69d714e39537597171453
Lukas <kosha.industry@gmail.com>
2024-05-14 12:20:07 +0500

    updates

--- lib/elevated_column.dart
+++ lib/elevated_column.dart
@@ -27,49 +27,4 @@ class ElevatedColumn extends ElevatedFlex {
   }): super(
     direction: Axis.vertical,
   );
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Widget> children;
-    if (direction == Axis.vertical && verticalDirection == VerticalDirection.up) {
-      children = this.children.reversed.toList();
-    } else {
-      children = this.children;
-    }
-
-    final Map<int, Widget> indexedChildren = children.asMap();
-
-    // sort children, move elevated widgets down
-    List<MapEntry<int, Widget>> sortedIndexedChildren = indexedChildren.entries.sorted((a, b) {
-      final aWidget = a.value;
-      final bWidget = b.value;
-
-      final aScore = aWidget is Elevated ? 1 : 0;
-      final bScore = bWidget is Elevated ? 1 : 0;
-
-      return aScore - bScore;
-    });
-
-    // convert children to BoxyId, but id saves their original order
-    final boxyChildren = sortedIndexedChildren.map((MapEntry<int, Widget> entry) {
-      final index = entry.key;
-      final child = entry.value;
-
-      return BoxyId(
-        id: index,
-        child: child,
-      );
-    }).toList();
-
-    return CustomBoxy(
-      delegate: _ElevatedFlexDelegate(
-        direction: direction,
-        mainAxisAlignment: mainAxisAlignment,
-        mainAxisSize: mainAxisSize,
-        crossAxisAlignment: crossAxisAlignment,
-        verticalDirection: verticalDirection,
-      ),
-      children: boxyChildren,
-    );
-  }
 }
