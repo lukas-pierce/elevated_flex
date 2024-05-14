dbbf9fda39d5420c6ac359d0fe2b7759bd79efb3
Lukas <kosha.industry@gmail.com>
2024-05-14 11:08:59 +0500

    updates

---
+++ lib/elevated_flex_base.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class ElevatedRow extends StatelessWidget {
+  const ElevatedRow({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.verticalDirection = VerticalDirection.down,
+    this.children = const <Widget>[],
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final VerticalDirection verticalDirection;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children;
+    if (direction == Axis.vertical && verticalDirection == VerticalDirection.up) {
+      children = this.children.reversed.toList();
+    } else {
+      children = this.children;
+    }
+
+    final Map<int, Widget> indexedChildren = children.asMap();
+
+    // sort children, move elevated widgets down
+    List<MapEntry<int, Widget>> sortedIndexedChildren = indexedChildren.entries.sorted((a, b) {
+      final aWidget = a.value;
+      final bWidget = b.value;
+
+      final aScore = aWidget is Elevated ? 1 : 0;
+      final bScore = bWidget is Elevated ? 1 : 0;
+
+      return aScore - bScore;
+    });
+
+    // convert children to BoxyId, but id saves their original order
+    final boxyChildren = sortedIndexedChildren.map((MapEntry<int, Widget> entry) {
+      final index = entry.key;
+      final child = entry.value;
+
+      return BoxyId(
+        id: index,
+        child: child,
+      );
+    }).toList();
+
+    return CustomBoxy(
+      delegate: _ElevatedRowDelegate(
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        verticalDirection: verticalDirection,
+      ),
+      children: boxyChildren,
+    );
+  }
+}
