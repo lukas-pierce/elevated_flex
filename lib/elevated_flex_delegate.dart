32b73d18432e5b536ffb72806ceea754d19df69a
Lukas <kosha.industry@gmail.com>
2024-05-14 14:29:45 +0500

    updates

--- lib/elevated_flex_delegate.dart
+++ lib/elevated_flex_delegate.dart
@@ -157,10 +157,17 @@ class _ElevatedFlexDelegate extends BoxyDelegate {
         double freeSpaceHeight = constraints.maxHeight - sumChildrenHeight;
 
         return switch (effectiveMainAxisAlignment) {
-          MainAxisAlignment.start => (startY: 0, stepShift: 0),
+          MainAxisAlignment.start => () {
+              return (startY: 0.0, stepShift: 0.0);
+            }(),
 
           // readability
-          MainAxisAlignment.end => (startY: freeSpaceHeight, stepShift: 0),
+          MainAxisAlignment.end => () {
+              return (
+                startY: freeSpaceHeight,
+                stepShift: 0.0,
+              );
+            }(),
 
           // readability
           MainAxisAlignment.center => () {
