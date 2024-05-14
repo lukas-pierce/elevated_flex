e8020a2e749a055904161a6364cb56b343501591
Lukas <kosha.industry@gmail.com>
2024-05-14 07:26:00 +0500

    add elevated.dart

---
+++ lib/elevated.dart
@@ -0,0 +1,10 @@
+class Elevated extends StatelessWidget {
+  const Elevated({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
