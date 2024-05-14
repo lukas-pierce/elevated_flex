75eaddd25950ca0a24a7a15ce13ea3146d510f9a
Lukas <kosha.industry@gmail.com>
2024-05-14 10:50:28 +0500

    updates

--- lib/elevated_row_delegate.dart
+++ lib/elevated_row_delegate.dart
@@ -2,18 +2,13 @@ part of 'elevated_flex.dart';
 
 class _ElevatedRowDelegate extends _ElevatedFlexDelegate {
   _ElevatedRowDelegate({
-    required this.mainAxisAlignment,
-    required this.mainAxisSize,
-    required this.crossAxisAlignment,
-    required this.verticalDirection,
+    required super.mainAxisAlignment,
+    required super.mainAxisSize,
+    required super.crossAxisAlignment,
+    required super.verticalDirection,
   }) : super(
           direction: Axis.horizontal,
         );
-
-  final MainAxisAlignment mainAxisAlignment;
-  final MainAxisSize mainAxisSize;
-  final CrossAxisAlignment crossAxisAlignment;
-  final VerticalDirection verticalDirection;
 
   // inner calculation
   late final int childrenCount;
