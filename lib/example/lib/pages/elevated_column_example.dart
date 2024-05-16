c17c81cc8222f29e4936639db547237671e58683
Lukas <kosha.industry@gmail.com>
2024-05-16 07:53:30 +0500

    updates

--- lib/example/lib/pages/elevated_column_example.dart
+++ lib/example/lib/pages/elevated_column_example.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ElevatedColExample extends StatelessWidget {
-  const ElevatedColExample({super.key});
+class ElevatedColumnExample extends StatelessWidget {
+  const ElevatedColumnExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('ElevatedColExample'),
+        child: Text('ElevatedColumnExample'),
       ),
     );
   }
