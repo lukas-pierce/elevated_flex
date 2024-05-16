00592896e6feb981f17f7b53411cf7010fa87c8a
Lukas <kosha.industry@gmail.com>
2024-05-16 07:40:56 +0500

    updates

---
+++ lib/example/lib/pages/elevated_row_example.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ElevatedRowExample extends StatelessWidget {
+  const ElevatedRowExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ElevatedRowExample'),
+      ),
+    );
+  }
+}
